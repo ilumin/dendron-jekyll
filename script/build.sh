9ff51fe46608830a53cdf010652644da57c8ce5e
kevinslin <kevin@thence.io>
2020-07-30 08:08:41 -0700

    chore: update build script

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,6 @@
 echo "Packaging gem... \n"
 gem build just-the-docs.gemspec
 
-echo "Cleaning up... \n"
-git add *.gem
-git commit -m 'Bump just-the-docs gem package'
+# echo "Cleaning up... \n"
+# git add *.gem
+# git commit -m 'Bump just-the-docs gem package'
