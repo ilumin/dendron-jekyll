b3bedbc5a89f41131253d5c2b4b4c2a40d4c2a60
kevinslin <kevin@thence.io>
2020-09-16 15:05:34 -0700

    chore: update build scripts

--- script/build.sh
+++ script/build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
+
+# TODO: bump version in just-the-docs.gemspec
+# set new version here
+version=0.3.6
 echo "Packaging gem... \n"
 gem build just-the-docs.gemspec
-
-# echo "Cleaning up... \n"
-# git add *.gem
-
-# git commit -m 'Bump just-the-docs gem package'
-
-# === Steps
-# bump version in just-the-docs.gemspec
-# gem build just-the-docs.gemspec
-# push to github
-# gem push {version | eg: dendron-jekyll-0.3.4.gem}
+git add .
+git commit -m "chore: publish new version"
+git push
+gem push dendron-jekyll-$version.gem
