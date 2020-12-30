3b00fecf8a530e0ea5480a4e2b78918421341b40
ilumin <iolumin@gmail.com>
2020-12-30 10:40:46 +0700

    add lunr thai language support

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -78,6 +78,7 @@ function initSearch() {
       lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
 
       var index = lunr(function(){
+        this.use(lunr.multiLanguage('en', 'th'));
         this.ref('id');
         this.field('hpath', { boost: 200 });
         this.field('title', { boost: 100 });
